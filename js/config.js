fb57fe58086a843cea72fa97e9b7b81aba75b8ca
Suraj Kumar <thesurajdev@gmail.com>
2026-04-04 14:29:40 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyaTkDRx7dejPy2KvAv599ItfZunT4q54p-2TZLqgm6J9yvu4wN_fJ3evzWvrPNGzXM/exec";
+const Deployment_ID = "AKfycbyaTkDRx7dejPy2KvAv599ItfZunT4q54p-2TZLqgm6J9yvu4wN_fJ3evzWvrPNGzXM";
