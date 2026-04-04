3d2e865b179368d67bb9dd17efbb6f1fa77ea5f6
Suraj Kumar <thesurajdev@gmail.com>
2026-04-04 17:23:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyI6t5hjypCiEJpzUqSyoOtQUgw9NN6XO7P67-8w4L42LtulNHHlNrbxUsQtZNNV1iI/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbws-7k-1VoaCFgWH07T2u_squu4saj6tAZlMh4o-boOlsIH5zWXcFco5XF7z0eegNEs/exec";
 /*const Deployment_ID = "AKfycbyaTkDRx7dejPy2KvAv599ItfZunT4q54p-2TZLqgm6J9yvu4wN_fJ3evzWvrPNGzXM";*/
